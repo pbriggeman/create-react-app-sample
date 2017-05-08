aa1b6e5689b9bc55cc8a1bb6237b245d53fdb608
Unknown <pbriggeman@gmail.com>
2017-05-08 19:54:05 -0400

    Added JQuery/Bootstrap, building home page view

---
+++ src/MainContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MainContainer.css';
+
+class MainContainer extends Component {
+  render() {
+    return (
+      <div className="maincontainer">
+        Main Container
+      </div>
+    );
+  }
+}
+
+export default MainContainer;
